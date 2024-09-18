27ffc3aa60a1fa6d772e28a17d5bbba1e57b9040
glbrtlian <glian6020@gmail.com>
2024-09-18 16:30:47 +0800

    pertemuan 3

---
+++ pertemuan 3/2409106110_Gilbert Finsen Zakaria lian_C2'24.py
@@ -0,0 +1,38 @@
+# cuaca = "mendung"
+# if cuaca == "mendung":
+#         print("diam di rumah")
+# else: 
+#         print ("hari ini mendung")          
+
+# if umur = int(input("masukan umur anda :"))
+#     if umur > 0:
+#         if umur <= 10:
+#         print("umur anda termasuk kategori anak-anak")
+#       elif umur <= 18:
+#         print("umur anda termasuk kategori remaja")
+#       elif umur <= 35:
+#         print("umur anda termasuk kategori dewasa")
+#       elif umur <=50
+#         print("umur anda termasuk paruh baya")
+#       else:
+#         print("umur anda termasuk kategori tua")
+# else:
+
+# nim = int(input("masukan nim"))
+
+# if nim >= 1 and nim <= 50 :
+#     if nim >= 1 and nim <= 25 :
+#         print("kelas A'1")
+#     else :
+#         print("kelas B'2")
+# elif nim >= 101 and nim < 121 :
+#     if nim >= 101 and nim <= 121 :
+#         print("kelas C'1")
+#     else :
+#         print("kelas C'2")
+# else : 
+#     print("anda bukan mahasiswa informatika 24")
+
+# nim = int(input("masukan nim"))
+# hasil = "kelas A" i nim >= 1 and nim <=50 else "kelas B" if nim >= 51 and nim <= 100 else "kelas C" if nim <= 121 else "mahasiswa siluman"
+# print(hasil)

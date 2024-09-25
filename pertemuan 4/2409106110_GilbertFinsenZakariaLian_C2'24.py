f9ac77bd53f6e45a69d7598244b5476bb305c915
glbrtlian <glian6020@gmail.com>
2024-09-25 16:27:38 +0800

    upload code pertemuan 4

---
+++ pertemuan 4/2409106110_GilbertFinsenZakariaLian_C2'24.py
@@ -0,0 +1,53 @@
+# ulang = 10
+# for i in range (ulang):
+#     print("perulangan ke-" + str(i))
+
+# for i in range (2,19) :
+#     print(f"halo{i}")
+
+# for i in range (2,10,2) :
+#     print(f"halo{i}")
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1 
+#     jawab = input("Ulang lagi tidak? ")
+#     print(f"Total perulangan: {hitung}")
+
+# i=1
+# while (i<=4):
+#     i = i + 1
+#     print("halo")
+
+# while True:
+#     print("hello world")
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+# while True:
+#     print("halo")
+#     break
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# kelasC = "lian", [2409106110]
+# for i in range(len(kelasC)):
+#     print(f"{i+1}) {kelasC[i]}")
+# print(kelasC)

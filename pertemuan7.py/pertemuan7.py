c53111635c23a5796d59bd2cbd88b02a5f5f850b
glbrtlian <glian6020@gmail.com>
2024-10-16 16:06:27 +0800

    Add files via upload

---
+++ pertemuan7.py/pertemuan7.py
@@ -0,0 +1,60 @@
+# # Membuat fungsi dengan parameter (panjang, lebar)
+# def luas_persegi_panjang(panjang, lebar):
+# luas = panjang * lebar
+# print ("Luas persegi panjang:", luas)
+# # Pemanggilan fungsi luas_persegi_panjang
+# luas_persegi_panjang(4, 6)
+
+# Contoh Program mengembalikan hasil fungsi
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     return luas
+
+# hasil= luas_persegi(4) = luas_persegi(2)
+# print(hasil)
+
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     jumlah= sisi + sisi
+#     return luas
+
+# luas, jumlah = luas_persegi
+# hasil = luas, jumlah
+
+# def mhs(nama,nim, **arr) :
+#     print(nama)
+#     print(nim)
+#     print(arr)
+
+# mhs("ucup", "20", data = 2)
+
+# def nilai(angka) :
+#     if angka > 10:
+#         print("1")
+#        # return
+#     else:
+#         print("2")
+#        # return
+#     print("3")
+
+# nilai(10)
+
+# data = 10 
+
+# def nilai(): 
+#     data=20
+#     print(data)
+
+# nilai()
+# print(data)
+
+# def data(kumpulan):
+    # print(kumpulan)
+          
+# valeu = [1,2,3,4]
+# data(valeu)
+
+def mhs(nama1, nama2, nama3):
+    print(nama3)
+
+mhs(nama3 = "ucup", nama2 = "saipul",nama1 = "randa")
